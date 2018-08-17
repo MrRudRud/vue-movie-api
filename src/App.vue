1b1d574f09c4f67ab5917183483147cb6f56d6d6
MrRudRud <mrrudrud@gmail.com>
2018-08-17 09:31:10 +0700

    conditional directives in Depth IF-ELSE

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,18 @@
 <template>
-  <div id="app">
+  <div id="app" v-if="status === 'Ready'">
     <Header v-bind:title="title"></Header>
-    <ul>
-      <li v-for="(item, index) in list">
-        {{ index + 1 }} - {{ item }}
-      </li>
-      <li v-bind:key="person.id" v-for="(person, index) in people">
-        {{ index + 1 }} - {{ person.name }}
-      </li>
-      <li v-bind:key="value.id" v-for="(value, key, index) in profile">
-        {{ index + 1 }} - {{ key }}:{{ value }}
-      </li>
-    </ul>
     <img alt="Vue logo" src="./assets/logo.png">
     <h1>{{hello}}</h1>
-    <HelloWorld v-for="person in people" v-bind:key="person.id"  msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+  <div v-else-if="status === 'Loading'">
+    Loading ...
+  </div>
+  <div v-else>
+    Error
   </div>
 </template>
-
+ 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import Header from './components/Header.vue'
@@ -28,17 +23,8 @@ export default {
     return {
       hello: "Hello World",
       title: "Vue Movie DB",
-      list: ["Scot", "Ryan", "Dino"],
-      people: [
-        { id: "1", name: 'RudRud' },
-        { id: "2", name: 'Rush' }
-      ],
-      profile: {
-        id: 1,
-        name: 'RudRud',
-        age: 32,
-        job: 'Web Developer'
-      }
+      isTrue: false,
+      status: 'Ready'
     };
   },
   components: {
